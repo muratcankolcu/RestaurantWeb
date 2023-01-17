3c54577014c1dfad9dfe1ee99e01d2e4c97dba32
muratcankolcu <92145567+muratcankolcu@users.noreply.github.com>
2023-01-18 00:57:23 +0300

    Restaurant Web

---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderDetailsService {
+  constructor() {}
+
+  //foodDetails
+
+  foodDetails = [
+    {
+      id: 1,
+      foodName: 'Paneer Grilled Sandwich',
+      foodDetails: 'Pan-fried masala paneer',
+      foodPrice: 200,
+      foodImage:
+        'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    },
+
+    {
+      id: 2,
+      foodName: 'Paneer Grilled Sandwich',
+      foodDetails: 'Pan-fried masala paneer',
+      foodPrice: 200,
+      foodImage:
+        'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    },
+
+    {
+      id: 3,
+      foodName: 'Paneer Grilled Sandwich',
+      foodDetails: 'Pan-fried masala paneer',
+      foodPrice: 200,
+      foodImage:
+        'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    },
+
+    {
+      id: 4,
+      foodName: 'Paneer Grilled Sandwich',
+      foodDetails: 'Pan-fried masala paneer',
+      foodPrice: 200,
+      foodImage:
+        'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    },
+
+    {
+      id: 5,
+      foodName: 'Paneer Grilled Sandwich',
+      foodDetails: 'Pan-fried masala paneer',
+      foodPrice: 200,
+      foodImage:
+        'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    },
+
+    {
+      id: 6,
+      foodName: 'Paneer Grilled Sandwich',
+      foodDetails: 'Pan-fried masala paneer',
+      foodPrice: 200,
+      foodImage:
+        'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    },
+  ];
+}
